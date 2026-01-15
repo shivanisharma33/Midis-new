5a60c3b7f6f060af5eced0f9f402ffa1c472853b
shividixit <shivani.midis@gmail.com>
2026-01-15 19:01:04 +0530

    add new section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -191,6 +191,12 @@ export default function Navbar() {
           <a href="/contact" className="px-4 py-2 hover:text-coral">
             Contact
           </a>
+              <a href="/Blogs" className="px-4 py-2 hover:text-coral">
+            Blogs
+          </a>
+           <a href="/contact" className="px-4 py-2 hover:text-coral">
+            Contact
+          </a>
         </div>
       </div>
 
